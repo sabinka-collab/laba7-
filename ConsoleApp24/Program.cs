61c819c28ba09764bc8fe0dab424fe7423518572
12 <12@DESKTOP-14UH3Q3>
2023-12-21 13:35:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,13 @@
+﻿string s1 = Console.ReadLine();
+string s2 = Console.ReadLine();
+char r1 = 'c';
+int i = 0;
+foreach (char s in s1)
+{
+    i++;
+    if (s == r1)
+    {
+        s1 = s1.Insert(i, s2);
+    }
+}
+Console.WriteLine(s1);
